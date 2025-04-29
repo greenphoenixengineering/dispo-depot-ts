53aebb121a54af6bb9335283ae6ee04c4899182b
elghanbibadr <bghanbi50@gmail.com>
2025-04-29 18:11:12 +0100

    make deletions on the edit buyer page

--- components/BuyersTable.tsx
+++ components/BuyersTable.tsx
@@ -172,9 +172,9 @@ export default function BuyersTable({
                     >
                       Edit
                     </Link>
-                    <button className="text-red-600 hover:text-red-900">
+                    {/* <button className="text-red-600 hover:text-red-900">
                       Delete
-                    </button>
+                    </button> */}
                   </td>
                 </tr>
               ))}
