76ae5e72e6e7bec6e612afc8b8d9e62601baa1a3
elghanbibadr <bghanbi50@gmail.com>
2025-05-06 17:49:00 +0100

    format the types files

--- libs/types.ts
+++ libs/types.ts
@@ -1,6 +1,6 @@
 export interface Buyer {
   id: number;
-  first_name: string
+  first_name: string;
   email: string;
   phone_num?: string;
   buyer_tags?: {
@@ -21,24 +21,16 @@ export interface buyerFormData {
 export interface NewBuyer extends buyerFormData {
   groupId: string;
 }
-
 export interface NewBuyerInSupa extends NewBuyer {
   api_id: string;
 }
-
-
-
-
 export interface UpdateBuyer {
-  buyerId:string
-  updates: buyerFormData
-  tags:any
-  buyerApiId:string
+  buyerId: string;
+  updates: buyerFormData;
+  tags: any;
+  buyerApiId: string;
 }
-
-
-
-export interface DeleteBuyer{
-buyerId:string
-buyerApiId:string
+export interface DeleteBuyer {
+  buyerId: string;
+  buyerApiId: string;
 }
