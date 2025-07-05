03b76b9fa330878d514c5910d8aa15fd263edf49
elghanbibadr <bghanbi50@gmail.com>
2025-07-05 19:00:55 +0100

    change app logo and name on the config

--- libs/next-auth.ts
+++ libs/next-auth.ts
@@ -97,7 +97,8 @@ export const authOptions: NextAuthOptionsExtended = {
     brandColor: config.colors.main,
     // Add you own logo below. Recommended size is rectangle (i.e. 200x50px) and show your logo + name.
     // It will be used in the login flow to display your logo. If you don't add it, it will look faded.
-    logo: `https://${config.domainName}/logoAndName.png`,
+        logo: `${process.env.NEXTAUTH_URL}/logo.svg`, 
+
   },
 };
 
