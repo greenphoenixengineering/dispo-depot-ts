c89055b61cce8380a8c7de56a8c6b41eb6c2a9a1
elghanbibadr <bghanbi50@gmail.com>
2025-07-05 15:00:17 +0100

    show only the tags that has a buyer count greater that 0 on the tag selection dropdown on the send deal page

--- app/dashboard/deals/page.tsx
+++ app/dashboard/deals/page.tsx
@@ -1,20 +1,31 @@
-import { getWholesalerTags } from "@/app/actions/supabase";
+import { getTagsWithCounts, getWholesalerTags } from "@/app/actions/supabase";
 import SendDealForm from "@/components/SendDealForm";
-import {  ArrowLeft } from "lucide-react";
-import Link from "next/link";
+import { TagWithBuyerCount } from "@/libs/tagTypes";
+
 import React from "react";
 
 const page = async () => {
   const tags = await getWholesalerTags();
+  const tagsWithBuyerCount:TagWithBuyerCount[] = await getTagsWithCounts();
+
+
+  const tagsThatHasBuyers=tagsWithBuyerCount?.filter(item=> item.buyer_count > 0)
+
+  console.log("tags with buyer count", tagsWithBuyerCount);
+  console.log("tags that has buyer", tagsThatHasBuyers);
 
 
   return (
     <>
       <div className="mb-6">
-          <h1 className="text-xl sm:text-2xl font-bold mb-2">Send Deals to Tags</h1>
-        <p className="text-sm sm:text-base text-gray-600">Create and send targeted deals to specific buyer segments</p>
+        <h1 className="text-xl sm:text-2xl font-bold mb-2">
+          Send Deals to Tags
+        </h1>
+        <p className="text-sm sm:text-base text-gray-600">
+          Create and send targeted deals to specific buyer segments
+        </p>
       </div>
-      <SendDealForm tags={tags} />
+      <SendDealForm tags={tagsThatHasBuyers} />
     </>
   );
 };
